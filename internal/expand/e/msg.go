9c5db9417b22d0a95a3a11a6d34c70ec3ed7d164
Meng-Xin <li997266754@gmail.com>
2023-06-03 18:27:51 +0800

    feat: 增添 ArticleDoseNotExist 文章不存在校验错误code码。

--- internal/expand/e/msg.go
+++ internal/expand/e/msg.go
@@ -1,11 +1,12 @@
 package e
 
 var ErrMsg = map[int]string{
-	SUCCESS:      "Ok",
-	ERROR:        "内部错误",
-	NotFindToken: "Token 不存在",
-	TokenExpired: "Token 已过期",
-	RefreshToken: "Token 无感刷新",
+	SUCCESS:             "Ok",
+	ERROR:               "内部错误",
+	NotFindToken:        "Token 不存在",
+	TokenExpired:        "Token 已过期",
+	RefreshToken:        "Token 无感刷新",
+	ArticleDoseNotExist: "文章不存在",
 }
 
 func GetMsg(code int) string {
