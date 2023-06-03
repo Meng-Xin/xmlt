ae70fd993aa9c07b0e8a59986b614413bb5a5e79
Meng-Xin <li997266754@gmail.com>
2023-06-03 18:29:55 +0800

    feat: 完善 Article Controller 层的注册方法

--- internal/api/v1/article.go
+++ internal/api/v1/article.go
@@ -4,7 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"strconv"
 	"xmlt/internal/domain"
+	"xmlt/internal/expand/e"
+	"xmlt/internal/expand/public"
 	"xmlt/internal/service"
 )
 
@@ -16,10 +19,32 @@ func NewArticleController(service service.ArticleService) *ArticleController {
 	return &ArticleController{service: service}
 }
 
-func (h *ArticleController) GetByID(ctx *gin.Context) {
-
+func (a *ArticleController) GetByID(ctx *gin.Context) {
+	code := e.SUCCESS
+	idStr := ctx.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		code = e.ArticleDoseNotExist
+		ctx.JSON(http.StatusOK, public.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+		})
+		return
+	}
+	art, err := a.service.Get(ctx.Request.Context(), id)
+	if err != nil {
+		// TODO 如果代码严谨的话，这边要区别是真的没有数据，还是服务器出现了异常
+		return
+	}
+	var vo ArticleVO
+	vo.init(art)
+	ctx.JSON(http.StatusOK, public.Response{
+		Status: code,
+		Data:   vo,
+	})
 }
 
+// Save 作者可以保存文章
 func (a *ArticleController) Save(ctx *gin.Context) {
 	var vo ArticleVO
 	if err := ctx.Bind(&vo); err != nil {
@@ -38,7 +63,7 @@ func (a *ArticleController) Save(ctx *gin.Context) {
 	if err != nil {
 		// 这边不能把 error 写回去
 		// 暂时我直接输出到控制台
-		log.Info(err)
+		log.Error(err)
 		ctx.String(http.StatusInternalServerError, "系统异常，请重试")
 		return
 	}
@@ -46,12 +71,20 @@ func (a *ArticleController) Save(ctx *gin.Context) {
 }
 
 type ArticleVO struct {
-	ID      uint64 `form:"id"`
-	Title   string `form:"title" binding:"required"`
-	Content string `form:"content" binding:"required"`
+	ID      uint64 `form:"id" json:"id"`
+	Title   string `form:"title" json:"title" binding:"required"`
+	Content string `form:"content" json:"content" binding:"required"`
 	// 一般来说，考虑到各种 APP 发版本不容易，
 	// 所以数字、货币、日期、国际化之类的都是后端做的
 	// 前端就是无脑展示
 	Ctime string
 	Utime string
+}
+
+func (a *ArticleVO) init(art domain.Article) {
+	a.ID = art.ID
+	a.Ctime = art.Ctime.String()
+	a.Utime = art.Utime.String()
+	a.Content = art.Content
+	a.Title = art.Title
 }
