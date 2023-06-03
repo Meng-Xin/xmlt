1d4ed6827933d8e524cd4a25c836f3e5cede2550
Meng-Xin <li997266754@gmail.com>
2023-06-03 12:19:11 +0800

    fix: 调整依赖注入位置顺序，并补充pprof包引入

--- main.go
+++ main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"net/http"
+	_ "runtime/pprof"
 	"xmlt/global"
 	"xmlt/initialize"
-	v1 "xmlt/internal/api/v1"
-	"xmlt/internal/repository"
-	"xmlt/internal/repository/cache"
-	"xmlt/internal/repository/dao"
-	"xmlt/internal/router"
-	"xmlt/internal/service"
 )
 
 func main() {
@@ -17,14 +12,8 @@ func main() {
 	initialize.GlobalInit()
 	// 开启pprof
 	go http.ListenAndServe("9191", nil)
-	// 依赖注入
-	artCache := cache.NewArticleRedisCache(global.Redis)
-	articleService := service.NewArticleService(
-		repository.NewArticleRepo(dao.NewArticleDAO(global.DB_ONLINE), nil),    // 加载线上库 - 用户
-		repository.NewArticleRepo(dao.NewArticleDAO(global.DB_MAKE), artCache), // 加载制作库 - 作者
-	)
-	v1.NewArticleController(articleService)
+
 	// 注册路由
-	r := router.NewRouter()
+	r := initialize.NewRouter()
 	r.Run(global.Config.Server.DSN())
 }
