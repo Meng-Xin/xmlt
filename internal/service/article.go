162f90f572362f45c68864cd6647c250c1754c1f
Meng-Xin <li997266754@gmail.com>
2023-06-03 18:29:25 +0800

    feat: 实现 Article 的 Service 层接口。

--- internal/service/article.go
+++ internal/service/article.go
@@ -6,32 +6,52 @@ import (
 	"xmlt/internal/repository"
 )
 
-type article struct {
+type ArticleService interface {
+	Save(ctx context.Context, article domain.Article) (domain.Article, error)
+	Publish(ctx context.Context, article domain.Article) (domain.Article, error)
+	Get(ctx context.Context, id uint64) (domain.Article, error)
+}
+
+type articleService struct {
 	bRepo repository.ArticleRepo // 作者库
 	cRepo repository.ArticleRepo // 线上库
 }
 
 func NewArticleService(bRepo repository.ArticleRepo, cRepo repository.ArticleRepo) ArticleService {
-	return &article{
+	return &articleService{
 		bRepo: bRepo,
 		cRepo: cRepo,
 	}
 }
 
 // Save 在 service 层面上才会有创建或者更新的概念。repository 的职责更加单纯一点
-func (a article) Save(ctx context.Context, article domain.Article) (domain.Article, error) {
-	//TODO implement me
-	panic("implement me")
+func (a *articleService) Save(ctx context.Context, article domain.Article) (domain.Article, error) {
+	if article.ID == 0 {
+		id, err := a.bRepo.Create(ctx, article)
+		if err != nil {
+			return domain.Article{}, err
+		}
+		article.ID = id
+		return article, nil
+	}
+	err := a.cRepo.Update(ctx, article)
+	return article, err
 }
 
 // Publish B 端 作者进行推送文章
-func (a article) Publish(ctx context.Context, article domain.Article) (domain.Article, error) {
-	//TODO implement me
-	panic("implement me")
+func (a *articleService) Publish(ctx context.Context, article domain.Article) (domain.Article, error) {
+	// 先保存作者库，在推送到线上库
+	art, err := a.Save(ctx, article)
+	if err != nil {
+		return domain.Article{}, err
+	}
+	// 同步给线上库，因为是不同的数据库无法使用事务，
+	// 只能考虑重试 + 监控 + 告警
+	_, err = a.cRepo.CreateAndCached(ctx, art)
+	return art, err
 }
 
 // Get 这是 C 端查看
-func (a article) Get(ctx context.Context, id uint64) (domain.Article, error) {
-	//TODO implement me
-	panic("implement me")
+func (a *articleService) Get(ctx context.Context, id uint64) (domain.Article, error) {
+	return a.cRepo.Get(ctx, id)
 }
