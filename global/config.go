c7e13461a721054a5cc85515ca8c6f4263b3c5f8
Meng-Xin <li997266754@gmail.com>
2023-07-01 09:59:42 +0800

    feat: 新增全局变量Log

--- global/config.go
+++ global/config.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-redis/redis"
 	"gorm.io/gorm"
 	"xmlt/config"
+	"xmlt/utils"
 )
 
 var (
@@ -11,4 +12,5 @@ var (
 	DB_MAKE   *gorm.DB          // 制作库 - 作家|审核
 	DB_ONLINE *gorm.DB          // 线上库 - 用户
 	Redis     *redis.Client     // RedisClient
+	Log       utils.ILog        // 异常日志系统
 )
