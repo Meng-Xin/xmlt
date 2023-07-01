137ee397de042f2033f9322133823017fe83b39c
Meng-Xin <li997266754@gmail.com>
2023-07-01 10:01:53 +0800

    feat: 新增日志系统初始化

--- initialize/init.go
+++ initialize/init.go
@@ -3,11 +3,14 @@ package initialize
 import (
 	"xmlt/config"
 	"xmlt/global"
+	"xmlt/utils"
 )
 
 func GlobalInit() {
 	// 初始化全局配置文件
 	global.Config = config.InitLoadConfig()
+	// 日志系统初始化
+	global.Log = utils.NewLLogger()
 	// gorm 初始化
 	InitDatabase(global.Config.MysqlMake.Dsn(), global.Config.MysqlOnline.Dsn())
 	// cache 初始化
