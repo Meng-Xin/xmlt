64d64243fb5bb016ebc0d84a1dc27585eb948594
Meng-Xin <li997266754@gmail.com>
2023-07-02 18:56:36 +0800

    fix: 新增UserRouter模块 初始化补充

--- internal/router/user.go
+++ internal/router/user.go
@@ -1,3 +1,28 @@
 package router
 
+import (
+	"github.com/gin-gonic/gin"
+	"xmlt/global"
+	v1 "xmlt/internal/api/v1"
+	"xmlt/internal/repository"
+	"xmlt/internal/repository/cache"
+	"xmlt/internal/repository/dao"
+	"xmlt/internal/service"
+)
+
 type UserRouter struct{}
+
+func (a *UserRouter) InitApiRouter(router *gin.RouterGroup) {
+	userRouter := router.Group("user")
+	// 依赖注入
+	userCache := cache.NewUserRedisCache(global.Redis)
+	userDao := dao.NewUserDao(global.DB_MAKE)
+	userService := service.NewUserService(
+		repository.NewUserRepo(userDao, userCache),
+	)
+	userCtrl := v1.NewUserController(userService)
+	{
+		userRouter.POST("/login", userCtrl.Login)
+		userRouter.POST("/register", userCtrl.Register)
+	}
+}
