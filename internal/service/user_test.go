d459cea558b7f78c56ffdd9808a625cdedb29674
Meng-Xin <li997266754@gmail.com>
2023-07-03 11:29:28 +0800

    fix: 调整user_test文件测试通过

--- internal/service/user_test.go
+++ internal/service/user_test.go
@@ -14,6 +14,7 @@ import (
 
 func TestUserService(t *testing.T) {
 	hashPwd := utils.MD5V("yyz", "salt", 10)
+	//inputUser := domain.User{UserName: "xxn", Password: "yyz", NickName: "小仙女郁郁症"}
 	testCases := []struct {
 		name      string
 		mock      func(ctrl *gomock.Controller) repository.UserRepo
@@ -23,18 +24,17 @@ func TestUserService(t *testing.T) {
 		wantErr  error
 	}{
 		{
-			name: "注册成功案例",
+			name:      "注册成功案例",
+			inputUser: domain.User{UserName: "xxn", Password: "yyz", NickName: "小仙女郁郁症"},
 			mock: func(ctrl *gomock.Controller) repository.UserRepo {
 				repo := repository.NewMockUserRepo(ctrl)
-				repo.EXPECT().GetByNickName(gomock.Any(), "xxn").Return(model.User{}, gorm.ErrRecordNotFound)
-				repo.EXPECT().GetByUserName(gomock.Any(), "小仙女郁郁症").Return(model.User{}, gorm.ErrRecordNotFound)
-				repo.EXPECT().Create(gomock.Any(), domain.User{
-					UserName: "xxn", Password: "yyz", NickName: "小仙女郁郁症",
-				}).Return(uint64(0), nil)
+				repo.EXPECT().GetByUserName(gomock.Any(), "xxn").Return(model.User{}, gorm.ErrRecordNotFound)
+				repo.EXPECT().GetByNickName(gomock.Any(), "小仙女郁郁症").Return(model.User{}, gorm.ErrRecordNotFound)
+				repo.EXPECT().Create(gomock.Any(), domain.User{UserName: "xxn", Password: hashPwd, NickName: "小仙女郁郁症"}).Return(uint64(1), nil)
 				return repo
 			},
-			inputUser: domain.User{UserName: "xxn", Password: "yyz", NickName: "小仙女郁郁症"},
-			wantUser:  domain.User{UserName: "xxn", Password: hashPwd, NickName: "小仙女郁郁症"},
+
+			wantUser: domain.User{ID: 1, UserName: "xxn", Password: hashPwd, NickName: "小仙女郁郁症"},
 		},
 	}
 
@@ -48,12 +48,12 @@ func TestUserService(t *testing.T) {
 			svc := NewUserService(repo)
 			// 下边进行 Service层接口调试
 			user, err := svc.Register(context.Background(), tc.inputUser)
-			// 成功案例
+			// 错误一致
 			assert.Equal(t, tc.wantErr, err)
 			if err != nil {
 				return
 			}
-			// 失败案例
+			// 数据一致
 			assert.Equal(t, tc.wantUser, user)
 		})
 	}
