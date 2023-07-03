6e77aac550f11b1a7c834e429535f11d2b39052d
Meng-Xin <li997266754@gmail.com>
2023-07-03 17:14:32 +0800

    feat: 新增 多个自定义code码和对应Text

--- internal/expand/e/code.go
+++ internal/expand/e/code.go
@@ -7,4 +7,7 @@ const (
 	TokenExpired        = 10011 // Token 已过期
 	RefreshToken        = 10012 // Token 无感刷新
 	ArticleDoseNotExist = 10013 // 文章不存在
+	UserExisted         = 10014 // 用户已存在
+	Registered          = 10015 // 账户已被注册
+	UserOrPasswordError = 10016 // 账号或密码错误
 )
--- internal/expand/e/msg.go
+++ internal/expand/e/msg.go
@@ -7,6 +7,9 @@ var ErrMsg = map[int]string{
 	TokenExpired:        "Token 已过期",
 	RefreshToken:        "Token 无感刷新",
 	ArticleDoseNotExist: "文章不存在",
+	UserExisted:         "昵称已存在",
+	Registered:          "账户已被注册",
+	UserOrPasswordError: "账号或密码错误",
 }
 
 func GetMsg(code int) string {
