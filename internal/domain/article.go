2aefbb28b68ef58e357bfa9f36cc9fc0820cebb9
Meng-Xin <li997266754@gmail.com>
2023-07-06 17:14:08 +0800

    fix: domain.Article 结构体补充字段

--- internal/domain/article.go
+++ internal/domain/article.go
@@ -3,11 +3,18 @@ package domain
 import "time"
 
 type Article struct {
-	ID      uint64
-	Title   string
-	Content string
-	Ctime   time.Time
-	Utime   time.Time
+	ID           uint64 // 帖子ID
+	Title        string // 帖子标题
+	Content      string // 帖子内容
+	CommentCount uint64 // 评论数量
+	Status       uint8  // 帖子状态
 
-	Author Author
+	Author      uint64 // 作者
+	CategoryID  uint16 // 所属板块
+	NiceTopic   uint8  // 精选话题
+	BrowseCount uint64 // 浏览量
+	ThumbsUP    uint32 // 点赞数
+
+	Ctime time.Time
+	Utime time.Time
 }
