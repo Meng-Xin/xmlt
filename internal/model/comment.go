aeb31b7ad061fe4149507ab91ec555b6ad3ce9bd
Meng-Xin <li997266754@gmail.com>
2023-07-06 17:18:06 +0800

    feat: 添加 评论表

---
+++ internal/model/comment.go
@@ -0,0 +1,14 @@
+package model
+
+// Comment 评论表
+type Comment struct {
+	ID        uint32 `gorm:"primaryKey;comment:评论ID"`
+	Content   string `gorm:"type:longtext;comment:评论内容"`
+	UserID    uint64 `gorm:"index;not null;comment:评论用户ID"`
+	ArticleID uint64 `gorm:"index;not null;comment:文章ID"`
+	Floor     uint32 `gorm:"index;not null;comment:评论楼层"`
+	State     uint8  `gorm:"comment:该评论状态"`
+
+	Ctime int64 // 创建时间，毫秒作为单位
+	Utime int64 // 更新时间，毫秒作为单位
+}
