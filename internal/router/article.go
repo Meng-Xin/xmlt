efd935424b3852f7255c3caf9cabbea620b129ee
Meng-Xin <li997266754@gmail.com>
2023-07-06 17:15:51 +0800

    fix: router.Article 添加鉴权中间件区分，补充Publish接口方法调用。

--- internal/router/article.go
+++ internal/router/article.go
@@ -9,25 +9,28 @@ import (
 	"xmlt/internal/repository/cache"
 	"xmlt/internal/repository/dao"
 	"xmlt/internal/service"
+	"xmlt/middle"
 )
 
 type ArticleRouter struct{}
 
 func (a *ArticleRouter) InitApiRouter(router *gin.RouterGroup) {
 	articleRouter := router.Group("article")
+	publicRouter := router.Group("article")
+	articleRouter.Use(middle.VerifyJWTMiddleware())
 	// 依赖注入
 	artCache := cache.NewArticleRedisCache(global.Redis)
 	articleService := service.NewArticleService(
-		repository.NewArticleRepo(dao.NewArticleDAO(global.DB_ONLINE), nil),    // 加载线上库 - 用户
-		repository.NewArticleRepo(dao.NewArticleDAO(global.DB_MAKE), artCache), // 加载制作库 - 作者
+		repository.NewArticleRepo(dao.NewArticleDAO(global.DB_ONLINE), artCache), // 加载线上库 - 用户
+		repository.NewArticleRepo(dao.NewArticleDAO(global.DB_MAKE), artCache),   // 加载制作库 - 作者
 	)
 	articleCtl := v1.NewArticleController(articleService)
 	{
-		articleRouter.GET("/read/:id", articleCtl.GetByID)
+		publicRouter.GET("/read/:id", articleCtl.GetByID)
 		articleRouter.GET("/new", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, "新建文章")
 		})
 		articleRouter.POST("/new", articleCtl.Save)
-		articleRouter.POST("/publish")
+		articleRouter.POST("/publish", articleCtl.Publish)
 	}
 }
