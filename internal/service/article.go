014e932a4385ad96961f16d2f251a7e853e1db67
Meng-Xin <li997266754@gmail.com>
2023-07-06 17:16:58 +0800

    fix: service.Article 调整 Save 和 Publish 接口方法返回值定义。

--- internal/service/article.go
+++ internal/service/article.go
@@ -3,18 +3,19 @@ package service
 import (
 	"context"
 	"xmlt/internal/domain"
+	"xmlt/internal/expand/e"
 	"xmlt/internal/repository"
 )
 
 type ArticleService interface {
-	Save(ctx context.Context, article domain.Article) (domain.Article, error)
-	Publish(ctx context.Context, article domain.Article) (domain.Article, error)
+	Save(ctx context.Context, article domain.Article) (uint64, error)
+	Publish(ctx context.Context, article domain.Article) error
 	Get(ctx context.Context, id uint64) (domain.Article, error)
 }
 
 type articleService struct {
-	bRepo repository.ArticleRepo // 作者库
-	cRepo repository.ArticleRepo // 线上库
+	bRepo repository.ArticleRepo // 线上库
+	cRepo repository.ArticleRepo // 作者库
 }
 
 func NewArticleService(bRepo repository.ArticleRepo, cRepo repository.ArticleRepo) ArticleService {
@@ -25,30 +26,40 @@ func NewArticleService(bRepo repository.ArticleRepo, cRepo repository.ArticleRep
 }
 
 // Save 在 service 层面上才会有创建或者更新的概念。repository 的职责更加单纯一点
-func (a *articleService) Save(ctx context.Context, article domain.Article) (domain.Article, error) {
+func (a *articleService) Save(ctx context.Context, article domain.Article) (uint64, error) {
+	// 保存文章到制作库
 	if article.ID == 0 {
-		id, err := a.bRepo.Create(ctx, article)
-		if err != nil {
-			return domain.Article{}, err
-		}
-		article.ID = id
-		return article, nil
+		return a.cRepo.Create(ctx, article)
 	}
-	err := a.cRepo.Update(ctx, article)
-	return article, err
+	dataInfo, err := a.cRepo.Get(ctx, article.ID)
+	if err != nil {
+		return 0, err
+	}
+	// 文章是否存在校验
+	if dataInfo.ID == 0 {
+		return 0, e.UpdateArticleNotFindError
+	}
+	// 本次更新作者是否合法
+	if dataInfo.Author != article.Author {
+		return 0, e.UpdateArticleIdenticalError
+	}
+	// 更新制作库
+	return article.ID, a.cRepo.Update(ctx, article)
+
 }
 
 // Publish B 端 作者进行推送文章
-func (a *articleService) Publish(ctx context.Context, article domain.Article) (domain.Article, error) {
+func (a *articleService) Publish(ctx context.Context, article domain.Article) error {
 	// 先保存作者库，在推送到线上库
-	art, err := a.Save(ctx, article)
+	id, err := a.Save(ctx, article)
 	if err != nil {
-		return domain.Article{}, err
+		return err
 	}
+	article.ID = id
 	// 同步给线上库，因为是不同的数据库无法使用事务，
 	// 只能考虑重试 + 监控 + 告警
-	_, err = a.cRepo.CreateAndCached(ctx, art)
-	return art, err
+	_, err = a.bRepo.CreateAndCached(ctx, article)
+	return err
 }
 
 // Get 这是 C 端查看
