62c7eff5562adf8b472bcee62a50ff1ee69f4212
Meng-Xin <li997266754@gmail.com>
2023-07-06 17:19:11 +0800

    feat: Errors 自定义错误：补充2个关于文章的错误定义。

--- internal/expand/e/errors.go
+++ internal/expand/e/errors.go
@@ -3,8 +3,10 @@ package e
 import "errors"
 
 var (
-	RedisInsertError     = errors.New("Reids - 插入失败")
-	UserNameExistedError = errors.New("该账号已被使用")
-	NickNameExistedError = errors.New("用户昵称已存在")
-	UserOrPwdError       = errors.New("账号或密码不存在")
+	RedisInsertError            = errors.New("Reids - 插入失败")
+	UserNameExistedError        = errors.New("该账号已被使用")
+	NickNameExistedError        = errors.New("用户昵称已存在")
+	UserOrPwdError              = errors.New("账号或密码不存在")
+	UpdateArticleNotFindError   = errors.New("不能推送不存在的文章")
+	UpdateArticleIdenticalError = errors.New("更新文章遇到不一致的作者ID")
 )
