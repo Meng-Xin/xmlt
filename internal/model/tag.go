732de9c7bf9040ea6fa3c3cf1641f080740bc4c8
Meng-Xin <li997266754@gmail.com>
2023-07-06 17:18:25 +0800

    feat: 添加 标签表

---
+++ internal/model/tag.go
@@ -0,0 +1,10 @@
+package model
+
+// Tag 标签表
+type Tag struct {
+	ID   uint16 `gorm:"primaryKey;comment:所属标签ID"`
+	Name string `gorm:"size:20;comment:标签名称"`
+
+	Ctime int64 // 创建时间，毫秒作为单位
+	Utime int64 // 更新时间，毫秒作为单位
+}
