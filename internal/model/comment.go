3297f594ceb434c5bcfdb5579f8d3f045f719dfb
Meng-Xin <li997266754@gmail.com>
2023-07-09 16:31:21 +0800

    fix: Comment 文章评论表 增添 ParentID 字段，实现多层级评论关系。

--- internal/model/comment.go
+++ internal/model/comment.go
@@ -2,10 +2,11 @@ package model
 
 // Comment 评论表
 type Comment struct {
-	ID        uint32 `gorm:"primaryKey;comment:评论ID"`
+	ID        uint64 `gorm:"primaryKey;comment:评论ID"`
 	Content   string `gorm:"type:longtext;comment:评论内容"`
 	UserID    uint64 `gorm:"index;not null;comment:评论用户ID"`
 	ArticleID uint64 `gorm:"index;not null;comment:文章ID"`
+	ParentID  uint64 `gorm:"index;not null;comment:父级评论ID"`
 	Floor     uint32 `gorm:"index;not null;comment:评论楼层"`
 	State     uint8  `gorm:"comment:该评论状态"`
 
