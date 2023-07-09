eb7cb74fb8943dc579e873baac745c5bf4e9c274
Meng-Xin <li997266754@gmail.com>
2023-07-09 19:10:28 +0800

    feat: 新增关于评论的错误定义

--- internal/expand/e/errors.go
+++ internal/expand/e/errors.go
@@ -9,4 +9,7 @@ var (
 	UserOrPwdError              = errors.New("账号或密码不存在")
 	UpdateArticleNotFindError   = errors.New("不能推送不存在的文章")
 	UpdateArticleIdenticalError = errors.New("更新文章遇到不一致的作者ID")
+	UpdateCommentIdentityError  = errors.New("更新评论状态身份错误！")
+	PagingMaxLimitError         = errors.New("分页获取超过限制")
+	PagingIsNullError           = errors.New("分页获取超过限制")
 )
