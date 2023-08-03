4842f912d905cde247a4e2577ad8ee2b2c18f3eb
Meng-Xin <li997266754@gmail.com>
2023-08-03 19:05:39 +0800

    fix：对定时任务进行拆分，使用差集优化程序结构。

--- internal/shared/crontask/sync_userlike.go
+++ internal/shared/crontask/sync_userlike.go
@@ -2,15 +2,16 @@ package crontask
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-co-op/gocron"
 	"github.com/go-redis/redis"
+	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 	"strconv"
 	"strings"
 	"xmlt/global"
 	"xmlt/internal/model"
 	"xmlt/internal/shared/enum"
+	"xmlt/utils"
 )
 
 type UserLikeCron interface {
@@ -27,66 +28,47 @@ type userLikeCron struct {
 	db       *gorm.DB
 }
 
+// SyncFromRedisToMysql 触发条件：1.Redis内至少存在一条有关点赞的记录信息，否则不加载。
 func (u *userLikeCron) SyncFromRedisToMysql() {
 	// 每 5 个小时从Redis同步到Mysql
-	u.schedule.Every(5).Hour().Do(func() {
+	u.schedule.Every(30).Second().Do(func() {
 		ctx := context.Background()
-		keys, err := u.client.WithContext(ctx).Keys("*").Result()
+		redisMarks, err := u.GetRedisMembers(ctx)
 		if err != nil {
-			panic(err)
+			global.Log.Warn(err.Error())
 		}
-		// 匹配符合要求的key
-		matchKey := make([]string, 0)
-		for i, _ := range keys {
-			if ok := strings.Contains(keys[i], enum.UserLikeArticle[:18]); ok {
-				matchKey = append(matchKey, keys[i])
-			}
-		}
-
-		// 遍历Redis集合并更新到MySQL
-		for i, _ := range matchKey {
-			endKey := strings.Split(matchKey[i], "_")
-			articleId, _ := strconv.ParseUint(endKey[3], 10, 64)
-			members, err := u.client.WithContext(ctx).ZRangeWithScores(matchKey[i], 0, -1).Result()
-
+		for artId, val := range redisMarks {
+			// key：uid Val: time ,本列表中的点赞信息记录
+			likeInfo := make(map[uint64]uint64, 0)
+			redisUsers := []uint64{}
+			mysqlUsers, err := u.GetMysqlMembers(ctx, artId)
 			if err != nil {
-				panic(err)
+				global.Log.Warn(err.Error())
 			}
-			for _, member := range members {
-				userId, _ := strconv.ParseUint(member.Member.(string), 10, 64)
-				score := member.Score
-				fmt.Printf("Member: %s, Score: %f\n", userId, score)
-				// 更新到Mysql
-				//userId, _ := strconv.ParseUint(memberValue, 10, 64)
-				entity := model.UserLikeArticle{
-					ArticleID: articleId,
-					UserID:    userId,
-					LikeState: true,
-					Ctime:     uint64(score),
-					Utime:     uint64(score),
+			for i, _ := range val {
+				// 获取完整uid列表
+				redisUsers = append(redisUsers, val[i].id)
+				// 拼接时间信息
+				likeInfo[val[i].id] = val[i].score
+			}
+			// 已存在于Mysql但是不存在于Redis
+			comparedRefMysql := utils.DifferenceCompared(mysqlUsers, redisUsers)
+			// 已存在于Redis但是不存在于MySQL
+			comparedRefRedis := utils.DifferenceCompared(redisUsers, mysqlUsers)
+			// comparedRefMysql 差集取消用户点赞状态
+			for _, uid := range comparedRefMysql {
+				err = u.UpdateUserLikeState(ctx, artId, uid, likeInfo[uid])
+				if err != nil {
+					log.Warn(err.Error())
 				}
-
-				// 开启事务从Redis更新数据到Mysql
-				u.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
-					daoUser := model.UserLikeArticle{}
-					err = tx.WithContext(ctx).Where("article_id=? and user_id=?", articleId, userId).First(&daoUser).Error
-					if err == nil && daoUser.ID != 0 {
-						//  TODO ArticleId相同前提下的所有 UserID，与Redis存储的Id取差集，把MySQL的点赞状态给取消掉；
-						err = tx.WithContext(ctx).Where("article_id=? and user_id=?", articleId, userId).Updates(&entity).Error
-						if err != nil {
-							panic(err)
-						}
-					} else {
-						// 执行创建
-						err = tx.WithContext(ctx).Model(model.UserLikeArticle{}).Where("article_id=? and user_id=?", articleId, userId).Save(&entity).Error
-						if err != nil {
-							panic(err)
-						}
-					}
-					return nil
-				})
 			}
-
+			// comparedRefRedis 差集补充用户点赞信息
+			for _, uid := range comparedRefRedis {
+				err = u.CreateUserLikeState(ctx, artId, uid, likeInfo[uid])
+				if err != nil {
+					log.Warn(err.Error())
+				}
+			}
 		}
 	})
 
@@ -98,7 +80,7 @@ type user struct {
 }
 
 // GetRedisMembers 从Redis获取点赞文章对应的结构
-func (u *userLikeCron) GetRedisMembers(ctx context.Context, client *redis.Client) map[uint64][]user {
+func (u *userLikeCron) GetRedisMembers(ctx context.Context) (map[uint64][]user, error) {
 	redisMark := make(map[uint64][]user, 0) // key: ArticleID Val: userIDs
 
 	keys, err := u.client.WithContext(ctx).Keys("*").Result()
@@ -119,7 +101,7 @@ func (u *userLikeCron) GetRedisMembers(ctx context.Context, client *redis.Client
 		articleId, _ := strconv.ParseUint(endKey[3], 10, 64)
 		members, err := u.client.WithContext(ctx).ZRangeWithScores(matchKey[i], 0, -1).Result()
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		users := make([]user, 0)
 		for _, member := range members {
@@ -128,20 +110,42 @@ func (u *userLikeCron) GetRedisMembers(ctx context.Context, client *redis.Client
 		}
 		redisMark[articleId] = users
 	}
-	return redisMark
+	return redisMark, nil
 }
 
 // GetMysqlMembers 从Mysql获取点赞用户信息
-func (u *userLikeCron) GetMysqlMembers(ctx context.Context, artId uint64) []uint64 {
+func (u *userLikeCron) GetMysqlMembers(ctx context.Context, artId uint64) ([]uint64, error) {
 	mysqlMark := make([]uint64, 0) // key: ArticleID Val: userIDs
 	daoUserLikes := []model.UserLikeArticle{}
 	err := u.db.WithContext(ctx).Where("article_id=?", artId).Find(&daoUserLikes).Error
 	if err != nil {
-		global.Log.Warn(err.Error())
-		return nil
+		return nil, err
 	}
 	for _, daoUser := range daoUserLikes {
 		mysqlMark = append(mysqlMark, daoUser.UserID)
 	}
-	return mysqlMark
+	return mysqlMark, nil
+}
+
+func (u *userLikeCron) UpdateUserLikeState(ctx context.Context, artId, uid uint64, timer uint64) error {
+	err := u.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		err := tx.WithContext(ctx).Where("article_id=? and user_id=?", artId, uid).Updates(&model.UserLikeArticle{LikeState: false, Utime: timer}).Error
+		return err
+	})
+
+	return err
+}
+
+func (u *userLikeCron) CreateUserLikeState(ctx context.Context, artId, uid uint64, timer uint64) error {
+	entity := model.UserLikeArticle{
+		ArticleID: artId,
+		UserID:    uid,
+		LikeState: true,
+		Ctime:     timer,
+	}
+	err := u.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		err := tx.WithContext(ctx).Model(model.UserLikeArticle{}).Create(&entity).Error
+		return err
+	})
+	return err
 }
