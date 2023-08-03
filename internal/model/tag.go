6cf3b7b6d8ad9a25a83823887a5943e423a1cf62
Meng-Xin <li997266754@gmail.com>
2023-08-03 19:21:32 +0800

    fix： 调整模型 Tag标签与Article为多对多关系。

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -2,8 +2,9 @@ package model
 
 // Tag 标签表
 type Tag struct {
-	ID   uint16 `gorm:"primaryKey;comment:所属标签ID"`
-	Name string `gorm:"size:20;comment:标签名称"`
+	ID       uint16    `gorm:"primaryKey;comment:所属标签ID"`
+	Name     string    `gorm:"size:20;comment:标签名称"`
+	Articles []Article `gorm:"many2many:article_tag"` // Tag : Article -> N : N
 
 	Ctime int64 // 创建时间，毫秒作为单位
 	Utime int64 // 更新时间，毫秒作为单位
