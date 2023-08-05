13ce5df87f257cfff8b635a1daa811c8857d76bc
Meng-Xin <li997266754@gmail.com>
2023-08-05 19:01:20 +0800

    docs: 修改部分结构体注释

--- internal/model/notification.go
+++ internal/model/notification.go
@@ -4,10 +4,11 @@ package model
 type Notification struct {
 	ID           uint64 `gorm:"primaryKey;comment:消息通知ID"`
 	Action       string `gorm:"size:50;comment:通知操作"`
-	SubjectID    uint8  `gorm:"comment:主题ID"`
+	SubjectID    uint8  `gorm:"comment:主题ID对应#IM、CommentId、ArticleId、LikeId"`
+	SubjectType  string `gorm:"size:10;comment:主题类型：IM(暂无)、COMMENT、PUSH、LIKE"`
 	AcceptUserID uint64 `gorm:"index;not null;comment:接收用户"`
 	SendUserID   uint64 `gorm:"index;not null;comment:发送用户"`
-
-	Ctime int64 // 创建时间，毫秒作为单位
-	Utime int64 // 更新时间，毫秒作为单位
+	State        bool   `gorm:"default:false;comment:是否查看"`
+	Ctime        int64  // 创建时间，毫秒作为单位
+	Utime        int64  // 更新时间，毫秒作为单位
 }
