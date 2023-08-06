61bf2fbbb211d2cf6e87975760ae320de6399ac7
Meng-Xin <li997266754@gmail.com>
2023-08-06 11:28:02 +0800

    fix: 规范化Api的Request和Response信息

--- internal/api/v1/article.go
+++ internal/api/v1/article.go
@@ -38,11 +38,12 @@ func (a *ArticleController) GetByID(ctx *gin.Context) {
 		// TODO 如果代码严谨的话，这边要区别是真的没有数据，还是服务器出现了异常
 		return
 	}
-	var vo ArticleSave
-	vo.init(art)
+	// 构建相应信息
+	var articleRes ArticleRes
+	articleRes.init(art)
 	ctx.JSON(http.StatusOK, public.Response{
 		Status: code,
-		Data:   vo,
+		Data:   articleRes,
 	})
 }
 
@@ -59,27 +60,19 @@ func (a *ArticleController) GetArticleByCate(ctx *gin.Context) {
 		return
 	}
 
-	var vo []ArticleSave
-	for i, _ := range arts {
-		articleVo := ArticleSave{}
-		articleVo.init(arts[i])
-		vo = append(vo, articleVo)
-	}
-
-	response := ArticleListResponse{
-		PageInfo: pageEntity,
-		Articles: vo,
-	}
+	// 构建相应信息
+	var articlesRes ArticlesRes
+	articlesRes.init(arts, pageEntity)
 	ctx.JSON(http.StatusOK, public.Response{
 		Status: code,
-		Data:   response,
+		Data:   articlesRes,
 	})
 }
 
 // Save 作者可以保存文章
 func (a *ArticleController) Save(ctx *gin.Context) {
 	code := e.SUCCESS
-	var data ArticleSave
+	var data ArticleSaveReq
 	if err := ctx.Bind(&data); err != nil {
 		// 出现 error 的情况下，实际上前端已经返回了
 		global.Log.Warn("解析Article Save 结构体错误：", err.Error())
@@ -87,7 +80,7 @@ func (a *ArticleController) Save(ctx *gin.Context) {
 	}
 	// 从 jwtmiddler 中拿到解析的UID
 	authorID := ctx.GetUint64(enum.CtxUid)
-	_, err := a.service.Save(ctx.Request.Context(), domain.Article{
+	artId, err := a.service.Save(ctx.Request.Context(), domain.Article{
 		Title:      data.Title,
 		Content:    data.Content,
 		CategoryID: data.CategoryID,
@@ -100,53 +93,88 @@ func (a *ArticleController) Save(ctx *gin.Context) {
 		ctx.String(http.StatusInternalServerError, "系统异常，请重试")
 		return
 	}
+
+	var articleSaveRes ArticleSaveRes
+	articleSaveRes.init(artId)
 	ctx.JSON(http.StatusOK, public.Response{
 		Status: code,
+		Data:   articleSaveRes,
 		Msg:    "保存成功！",
 	})
 }
 
 func (a *ArticleController) Publish(ctx *gin.Context) {
+	// TODO 跨领域调用，作者发布后需要通过,Notification模块进行消息推送。
 	code := e.SUCCESS
-
-	var data ArticleSave
+	var data ArticlePublishReq
 	err := ctx.Bind(&data)
 	if err != nil {
+		code = e.ERROR
 		global.Log.Warn("解析Article Save 结构体错误：", err.Error())
+		ctx.JSON(http.StatusOK, public.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+		})
 		return
 	}
-	// CheckPushPermission 检查权限
 	authorID := ctx.GetUint64(enum.CtxUid)
-	err = a.service.Publish(ctx, domain.Article{
-		ID:         data.ID,
-		Title:      data.Title,
-		Content:    data.Content,
-		CategoryID: data.CategoryID,
-		Author:     authorID,
-	})
+	publishId, err := a.service.Publish(ctx, data.ArticleId, authorID)
 	if err != nil {
-		if err == e.UpdateArticleIdenticalError {
-			global.Log.Warn("非法修改文章作者身份,记录IP：", ctx.Request.Host)
-		}
+		code = e.ERROR
+		global.Log.Warn(err.Error())
+		ctx.JSON(http.StatusOK, public.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+		})
 		return
 	}
+	var articlePublishRes ArticlePublishRes
+	articlePublishRes.init(publishId)
 	ctx.JSON(http.StatusOK, public.Response{
 		Status: code,
+		Data:   articlePublishRes,
 		Msg:    "发布成功，审核中……",
 	})
 }
 
-type ArticleSave struct {
-	ID          uint64 `form:"id" json:"id"`
-	Title       string `form:"title" json:"title" binding:"required"`           // 帖子标题
-	Content     string `form:"content" json:"content" binding:"required"`       // 帖子内容
-	Status      uint8  `form:"status" json:"status"`                            // 帖子状态
-	CategoryID  uint64 `json:"categoryID" form:"categoryID" binding:"required"` // 所属板块
-	NiceTopic   uint8  `json:"niceTopic" from:"niceTopic"`                      // 精选话题
-	BrowseCount uint64 `json:"browseCount" from:"browseCount"`                  // 浏览量
-	ThumbsUP    uint64 `json:"thumbsUP" form:"thumbsUP"`                        // 点赞数
-	Author      uint64 // 作者
-	UserInfo    struct {
+// ArticleSaveReq POST:/article/new
+type ArticleSaveReq struct {
+	Title      string `form:"title" json:"title" binding:"required"`           // 帖子标题
+	Content    string `form:"content" json:"content" binding:"required"`       // 帖子内容
+	CategoryID uint64 `form:"categoryID" json:"categoryID" binding:"required"` // 所属板块
+}
+
+type ArticleSaveRes struct {
+	ArticleId uint64 `from:"article_id" json:"id"`
+}
+
+func (a *ArticleSaveRes) init(artId uint64) {
+	a.ArticleId = artId
+}
+
+type ArticlePublishReq struct {
+	ArticleId uint64 `form:"article_id" json:"article_id"`
+}
+
+type ArticlePublishRes struct {
+	ArticleId uint64
+}
+
+func (a *ArticlePublishRes) init(artId uint64) {
+	a.ArticleId = artId
+}
+
+// ArticleRes GetByID -> Get:article/:id
+type ArticleRes struct {
+	ID          uint64   // 帖子ID
+	Title       string   // 帖子标题
+	Content     string   // 帖子内容
+	Status      uint8    // 帖子状态
+	CategoryID  uint64   // 所属板块
+	NiceTopic   uint8    // 精选话题
+	BrowseCount uint64   // 浏览量
+	ThumbsUP    uint64   // 点赞数
+	Author      struct { // 作者信息
 		ID       uint64
 		NickName string
 		Avatar   string
@@ -155,7 +183,7 @@ type ArticleSave struct {
 	Utime string
 }
 
-func (a *ArticleSave) init(art domain.Article) {
+func (a *ArticleRes) init(art domain.Article) {
 	a.ID = art.ID
 	a.Title = art.Title
 	a.Content = art.Content
@@ -164,15 +192,24 @@ func (a *ArticleSave) init(art domain.Article) {
 	a.NiceTopic = art.NiceTopic
 	a.BrowseCount = art.BrowseCount
 	a.ThumbsUP = art.ThumbsUP
-	a.Author = art.Author
-	a.UserInfo.ID = art.User.ID
-	a.UserInfo.NickName = art.User.NickName
-	a.UserInfo.Avatar = art.User.Avatar
+	a.Author.ID = art.User.ID
+	a.Author.NickName = art.User.NickName
+	a.Author.Avatar = art.User.Avatar
 	a.Ctime = art.Ctime.String()
 	a.Utime = art.Utime.String()
 }
 
-type ArticleListResponse struct {
-	PageInfo *shared.Page  // 分页信息
-	Articles []ArticleSave // 文章列表
+// ArticlesRes GetArticleByCate -> Get：article/read?category=
+type ArticlesRes struct {
+	PageInfo *shared.Page // 分页信息
+	Articles []ArticleRes // 文章列表
+}
+
+func (a *ArticlesRes) init(arts []domain.Article, page *shared.Page) {
+	for _, art := range arts {
+		var article ArticleRes
+		article.init(art)
+		a.Articles = append(a.Articles, article)
+	}
+	a.PageInfo = page
 }
